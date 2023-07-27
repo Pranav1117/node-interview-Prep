f06e22e9fd2327fa412de2a4a1be7813a4c14e2e
pranav <pranavbavaskar1@gmail.com>
2023-07-27 10:14:45 +0530

    day2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const http = require("http");
+//implementation of creating http server
+
+/*const http = require("http");
 
 const app = http.createServer((req, res) => {
   const url = req.url;
@@ -7,6 +9,40 @@ const app = http.createServer((req, res) => {
     res.write(JSON.stringify({ name: "harry" }));
     res.end();
   }
+});
+
+app.listen(3000, () => {
+  console.log("running on 3000");
+});
+*/
+
+// Implementation of Express
+
+const express = require("express");
+const app = express();
+
+const auth = (req, res, next) => {
+  console.log("specific middleware on home page");
+  if (req.query.age > 18) {
+    next();
+  } else {
+    res.send("user not eligible");
+  }
+};
+
+const global = (req, res, next) => {
+  console.log("global middleware");
+  next();
+};
+
+app.use(global);
+
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
+
+app.get("/checkAge", auth, (req, res) => {
+  res.send("user eligible");
 });
 
 app.listen(3000, () => {
